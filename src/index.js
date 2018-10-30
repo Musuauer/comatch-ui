002e25116f2314b7c31b50b6720673652a57e3d1
Yiotis Kaltsikis <giotisgr@gmail.com>
2018-10-30 09:11:28 +0100

    Remove serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 import { App } from './core/ui/components/App';
 import { importFonts } from './core/styles/fonts';
 
@@ -23,11 +22,6 @@ async function render(Component) {
 }
 
 render(App);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 if (IS_DEVELOPMENT && module.hot) {
