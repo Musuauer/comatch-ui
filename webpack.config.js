b1d37edfe44cd40c75ecdbf44a03fae910cf5ccb
Yiotis Kaltsikis <giotisgr@gmail.com>
2018-10-30 15:25:50 +0100

    Config project on npm to function as library

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'react-core.js',
+        library: '',
+        libraryTarget: 'commonjs',
+    },
+    externals: [nodeExternals()],
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
                 },
             },
             {
