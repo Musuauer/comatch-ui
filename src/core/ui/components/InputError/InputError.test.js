1942a092dc9ff077808074d2ca59efb52e037708
Stanimir Stoychev <xstukixs@gmail.com>
2019-01-15 16:53:42 +0100

    Adding tests to the InputLabel
    * Code review related changes

--- src/core/ui/components/InputError/InputError.test.js
+++ src/core/ui/components/InputError/InputError.test.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { InputError } from './InputError';
 import '../../../config/tests/setup';
 
-const testText = 'Testing InputError';
-
 describe('InputError', () => {
+    const testText = 'Testing InputError';
+
     it('should render correctly', () => {
         const inputError = renderer.create(<InputError text={testText} />).toJSON();
         expect(inputError).toMatchSnapshot();
     });
 
-    it('should render an InputError with default props', () => {
-        const item = shallow(<InputError text={testText} />);
+    it('should render an InputError with properly mapped props', () => {
+        const inputError = mount(<InputError text={testText} />);
 
-        // should have a default classe .InputError
-        expect(item.hasClass('InputError')).toEqual(true);
-    });
-
-    it('should render an InputError with specified text', () => {
-        const item = shallow(<InputError text={testText} />);
-
-        expect(item.text()).toEqual(testText);
+        expect(inputError.prop('text')).toEqual(testText);
+        expect(inputError.text()).toEqual(testText);
     });
 });
