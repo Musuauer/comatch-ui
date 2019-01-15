52db49dbbe1374cc89f89cf398123a8dceb76836
Stanimir Stoychev <xstukixs@gmail.com>
2019-01-15 12:49:46 +0100

    InputError tests
    * Tests to cover - rendering, default props, and passed props (text)

---
+++ src/core/ui/components/InputError/InputError.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import { InputError } from './InputError';
+import '../../../config/tests/setup';
+
+const testText = 'Testing InputError';
+
+describe('InputError', () => {
+    it('should render correctly', () => {
+        const inputError = renderer.create(<InputError text={testText} />).toJSON();
+        expect(inputError).toMatchSnapshot();
+    });
+
+    it('should render an IputError with default props', () => {
+        const item = shallow(<InputError text={testText} />);
+
+        // should have a default classe .InputError
+        expect(item.hasClass('InputError')).toEqual(true);
+    });
+
+    it('should render an InputError with specified text', () => {
+        const item = shallow(<InputError text={testText} />);
+
+        expect(item.text()).toEqual(testText);
+    });
+});
