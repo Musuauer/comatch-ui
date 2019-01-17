a47d3978d9a5744fa5729cf3eaf805afe8eb1b2d
Alicja <a.dul@comatch.com>
2019-01-17 15:57:55 +0100

    tests
    Adding tests for default props
    Adding test for test prop

--- src/core/ui/components/InputLabel/InputLabel.test.js
+++ src/core/ui/components/InputLabel/InputLabel.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
 
 import { InputLabel } from './InputLabel';
 import '../../../config/tests/setup';
@@ -8,34 +8,52 @@ import '../../../config/tests/setup';
 const text = 'A very random text';
 
 describe('InputLabel', () => {
-
     it('should render correctly', () => {
-        const inputLabel = renderer.create(<InputLabel  text={text} />).toJSON();
+        const inputLabel = create(<InputLabel />).toJSON();
+        expect(inputLabel).toMatchSnapshot();
+    });
+    it('snapshot of span prop set to true', () => {
+        const inputLabel = create(<InputLabel span />).toJSON();
         expect(inputLabel).toMatchSnapshot();
     });
 
-    let item;
-    beforeEach(() => {
-        item = shallow(<InputLabel text={text}/>);
-    })
+    it('should render the component with text passed as props', () => {
+        const item = shallow(<InputLabel text={text} />);
+        expect(item.find('label').text()).toEqual(text);
+    });
+
+    it('should render default props if extra props are being passed', () => {
+        const item = mount(<InputLabel text={text} />);
+        expect(item.prop('htmlFor')).toEqual(null);
+        expect(item.prop('id')).toEqual(null);
+        expect(item.prop('span')).toEqual(false);
+        expect(item.prop('className')).toEqual(null);
+        expect(item.prop('required')).toEqual(false);
+    });
+
     it('should render InputLabel as a default class', () => {
+        const item = shallow(<InputLabel text={text} />);
         expect(item.hasClass('InputLabel')).toEqual(true);
     });
-    it('should render InputLabel with extra className if passed by the props', () => {
-        const className = 'A very random className';
-        item.setProps({ className });
-        expect(item.hasClass(className)).toEqual(true);
+    it('should render InputLabel with extra classNames if passed by the props', () => {
+        const item = shallow(<InputLabel text={text} />);
+        const classNames = 'error with-border';
+        item.setProps({ className: classNames });
+        expect(item.hasClass(classNames)).toEqual(true);
     });
     it('should have asterisk after the text if the required prop is set to true', () => {
+        const item = shallow(<InputLabel text={text} />);
         item.setProps({ required: true });
         expect(item.find('label').text()).toEqual(`${text}*`);
     });
     it('should render a <span> instead of <label> if span prop is set to true', () => {
+        const item = shallow(<InputLabel text={text} />);
         item.setProps({ span: true });
         expect(item.find('label').length).toEqual(0);
         expect(item.find('span').length).toEqual(1);
     });
     it('should set an id attribute if the id prop is given', () => {
+        const item = shallow(<InputLabel text={text} />);
         const id = 'A very random id';
         item.setProps({ id });
         expect(item.prop('id')).toEqual(id);
