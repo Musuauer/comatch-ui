4ccd9f61de90889638fe8b5e0f46be07a74c1ac2
Stanimir Stoychev <xstukixs@gmail.com>
2019-01-28 08:51:47 +0100

    Code review

--- src/core/ui/components/Badge/StyledWrapper.js
+++ src/core/ui/components/Badge/StyledWrapper.js
@@ -9,7 +9,7 @@ export const getStylesBasedOnSize = ({ size }) => {
                 font-weight: bold;
                 line-height: 1;
                 padding: 2px 5px 3px 5px;
-        
+
                 svg {
                     height: 10px;
                 }
