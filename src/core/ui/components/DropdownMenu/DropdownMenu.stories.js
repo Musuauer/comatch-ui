c6698d1bafdb1895b893ddd6240035c8a899304d
Stanimir Stoychev <xstukixs@gmail.com>
2019-01-28 08:49:24 +0100

    Code Review
    * Added a story with an empty list of items (default prop)

--- src/core/ui/components/DropdownMenu/DropdownMenu.stories.js
+++ src/core/ui/components/DropdownMenu/DropdownMenu.stories.js
@@ -7,9 +7,16 @@ import { DropdownMenu } from './DropdownMenu';
 const dropdownMenuItems = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
 const dropdownMenuItemsString = `[${dropdownMenuItems.map((item) => `"${item}"`).join(',')}]`;
 
-storiesOf('DropdownMenu', module).add('with basic list of items', () => (
-    <>
-        <DropdownMenu items={dropdownMenuItems} />
-        <Highlight className="html">{`<DropdownMenu items={${dropdownMenuItemsString}} />`}</Highlight>
-    </>
-));
+storiesOf('DropdownMenu', module)
+    .add('with an empty list of items', () => (
+        <>
+            <DropdownMenu />
+            <Highlight className="html">{`<DropdownMenu />`}</Highlight>
+        </>
+    ))
+    .add('with basic list of items', () => (
+        <>
+            <DropdownMenu items={dropdownMenuItems} />
+            <Highlight className="html">{`<DropdownMenu items={${dropdownMenuItemsString}} />`}</Highlight>
+        </>
+    ));
