b47ee43a97db4e890550c47d033210b14d18670f
Stanimir Stoychev <xstukixs@gmail.com>
2019-01-30 14:49:52 +0100

    Code review changes

--- src/core/ui/components/DropdownMenu/StyledWrapper.js
+++ src/core/ui/components/DropdownMenu/StyledWrapper.js
@@ -2,18 +2,19 @@ import styled from 'styled-components';
 import { palette } from '../../../styles/variables';
 
 export const ItemStyledWrapper = styled.div`
+    background-color: transparent;
     color: ${palette.black};
     padding: 5px 10px;
+    transition: background-color 250ms ease-out;
 
     &:hover {
         background-color: ${palette.lightGray};
-        transition: background-color 250ms ease-out;
     }
 `;
 
 export const StyledWrapper = styled.div`
-    overflow: hidden;
-    border-radius: 5px;
     background-color: ${palette.white};
+    border-radius: 5px;
     box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.5);
+    overflow: hidden;
 `;
