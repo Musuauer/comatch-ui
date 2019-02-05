1ed3c61ae706a6abebf78d1b623a9b02d143cc58
Alicja <a.dul@comatch.com>
2019-02-05 17:07:02 +0100

    Fixing the structure of test blocks

--- src/core/ui/components/Popover/Popover.test.js
+++ src/core/ui/components/Popover/Popover.test.js
@@ -19,83 +19,66 @@ describe('Popover', () => {
         expect(popover.prop('open')).toEqual(null);
     });
 
-    describe('passed props', () => {
-        describe('custom toggel', () => {
-            it('should render correctly', () => {
-                const customToggle = <button icon='customIcon'>Hover me</button>;
-                const popover = mount(<Popover toggle={customToggle} />);
-                const button = popover.find('button');
+    it('should have correct passed props', () => {
+        const toggle = <button icon='customIcon'>Hover me</button>;
+        const children = <div />;
+        const popover = mount(<Popover position='left' toggle={toggle} open >{children}</Popover>);
 
-                expect(button.prop('icon')).toEqual('customIcon');
-                expect(button.text()).toEqual('Hover me');
-            });
-        });
+        expect(popover.prop('position')).toEqual('left');
+        expect(popover.prop('toggle')).toEqual(toggle);
+        expect(popover.prop('open')).toEqual(true);
+        expect(popover.prop('children')).toEqual(children);
+    })
 
-        describe('position', () => {
-            it('should render properly', () => {
-                const popover = mount(<Popover position='left' />);
-                expect(popover.prop('position')).toEqual('left');
-            });
+    describe('when the prop open is not null thus the component is controlled', () => {
+        it('should not fire handleMouseEnter() & handleMouseLeave()', () => {
+            const popover = mount(<Popover open={!null}>Ala</Popover>);
+            const section = popover.find('section');
+            const spyMouseEnter = jest.spyOn(popover.instance(), 'handleMouseEnter');
+            const spyMouseLeave = jest.spyOn(popover.instance(), 'handleMouseLeave');
+
+            section.simulate('mouseEnter', spyMouseEnter);
+            expect(spyMouseEnter).not.toHaveBeenCalled();
+
+            section.simulate('mouseLeave', spyMouseLeave);
+            expect(spyMouseLeave).not.toHaveBeenCalled();
         });
     });
 
-    describe('mouse events', () => {
-        describe('controlledByExternalState true', () => {
-            it('should not fire handleMouseEnter() & handleMouseLeave()', () => {
-                const popover = mount(<Popover open={!null}>Ala</Popover>);
-                const section = popover.find('section');
-                const spyMouseEnter = jest.spyOn(popover.instance(), 'handleMouseEnter');
-                const spyMouseLeave = jest.spyOn(popover.instance(), 'handleMouseLeave');
-
-                section.simulate('mouseEnter', spyMouseEnter);
-                expect(spyMouseEnter).not.toHaveBeenCalled();
-
-                section.simulate('mouseLeave', spyMouseLeave);
-                expect(spyMouseLeave).not.toHaveBeenCalled();
-            });
+    describe('when the prop open is null thus the component is not controlled', () => {
+        let popover;
+        let section;
+        beforeEach(() => {
+            popover = mount(<Popover open={null} />);
+            section = popover.find('section');
         });
 
-        describe('controlledByExternalState false', () => {
-            let popover;
-            let section;
-            beforeEach(() => {
-                popover = mount(<Popover open={null} />);
-                section = popover.find('section');
-            });
+        it('should fire handleMouseEnter(), handleMouseLeave() & handleTransitionEnd()', () => {
+            const spyMouseEnter = jest.spyOn(popover.instance(), 'handleMouseEnter');
+            const spyMouseLeave = jest.spyOn(popover.instance(), 'handleMouseLeave');
+            const spyTransitionEnd = jest.spyOn(popover.instance(), 'handleTransitionEnd')
+            popover.instance().forceUpdate();
 
-            it('should fire handleMouseEnter(), handleMouseLeave() & handleTransitionEnd()', () => {
-                const spyMouseEnter = jest.spyOn(popover.instance(), 'handleMouseEnter');
-                const spyMouseLeave = jest.spyOn(popover.instance(), 'handleMouseLeave');
-                const spyTransitionEnd = jest.spyOn(popover.instance(), 'handleTransitionEnd')
-                popover.instance().forceUpdate();
+            section.simulate('mouseEnter', spyMouseEnter);
+            expect(spyMouseEnter).toHaveBeenCalled();
 
-                section.simulate('mouseEnter', spyMouseEnter);
-                expect(spyMouseEnter).toHaveBeenCalled();
+            section.simulate('mouseLeave', spyMouseLeave);
+            expect(spyMouseLeave).toHaveBeenCalled();
 
-                section.simulate('mouseLeave', spyMouseLeave);
-                expect(spyMouseLeave).toHaveBeenCalled();
+            section.simulate('transitionEnd');
+            expect(spyTransitionEnd).toHaveBeenCalled();
+        });
 
-                section.simulate('transitionEnd');
-                expect(spyTransitionEnd).toHaveBeenCalled();
-            });
+        it('should set open and renderContent to true on mouseEnter', () => {
+            section.simulate('mouseEnter');
+            expect(popover.state('open')).toBe(true);
+            expect(popover.state('renderContent')).toBe(true);
+        });
 
-            it('should set open and renderContent to true on mouseEnter', () => {
-                section.simulate('mouseEnter');
-                expect(popover.state('open')).toBe(true);
-                expect(popover.state('renderContent')).toBe(true);
-            });
-
-            it('should set open and renderContent to false on mouseLeave', () => {
-                section.simulate('mouseLeave');
-                expect(popover.state('open')).toBe(false);
-                expect(popover.state('renderContent')).toBe(false);
-            });
-
-            it('should set open and renderContent to false on mouseLeave', () => {
-                section.simulate('mouseLeave');
-                expect(popover.state('open')).toBe(false);
-                expect(popover.state('renderContent')).toBe(false);
-            });
+        it('should set open and renderContent to false on mouseLeave', () => {
+            section.simulate('mouseLeave');
+            expect(popover.state('open')).toBe(false);
+            expect(popover.state('renderContent')).toBe(false);
         });
     });
 });
