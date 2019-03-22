21e03b6813026eb69a8af0b7e0b41ae7e0f66a83
Stanimir Stoychev <xstukixs@gmail.com>
2019-03-22 09:55:25 +0100

    InputLabel - converted to a styled component

--- src/core/ui/components/InputLabel/InputLabel.stories.js
+++ src/core/ui/components/InputLabel/InputLabel.stories.js
@@ -2,10 +2,28 @@ import React from 'react';
 import Highlight from 'react-highlight';
 import { storiesOf } from '@storybook/react';
 import { InputLabel } from './InputLabel';
+import { TextInput } from '../TextInput';
 
-storiesOf('InputLabel', module).add('with basic style', () => (
-    <>
-        <InputLabel text="Sample label" />
-        <Highlight className="html">{'<InputLabel text="Sample label" />'}</Highlight>
-    </>
-));
+storiesOf('InputLabel', module)
+    .add('with basic style', () => (
+        <>
+            <InputLabel text="Sample label" />
+            <Highlight className="html">{'<InputLabel text="Sample label" />'}</Highlight>
+        </>
+    ))
+    .add('as a span', () => (
+        <>
+            <InputLabel span text="Sample label" />
+            <Highlight className="html">{'<InputLabel span text="Sample label" />'}</Highlight>
+        </>
+    ))
+    .add('with a "for" attribute', () => (
+        <>
+            <InputLabel text="Sample label" htmlFor="input" />
+            <TextInput id="input" />
+            <Highlight className="html">
+                {'<InputLabel span text="Sample label" />'}
+                {'\n<TextInput id="input" />'}
+            </Highlight>
+        </>
+    ));
